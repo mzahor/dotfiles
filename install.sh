948f3e24c08e93786a5e1aa54826cadf363d2259
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2015-04-22 00:15:39 +0300

    make a hardlink of config files instead of copying

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp .vimrc ~/
-cp .tmux.conf ~/
+ln -b .vimrc ~/.vimrc
+ln -b .tmux.conf ~/.tmux.conf
 ./git_config.sh
 
