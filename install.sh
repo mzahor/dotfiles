a4bd9b5304b6534815f0496e192574c307b20a49
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2024-03-09 20:08:12 +0200

    feat: slight update

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -b .vimrc ~/.vimrc
 ln -b .tmux.conf ~/.tmux.conf
 ./git_config.sh
